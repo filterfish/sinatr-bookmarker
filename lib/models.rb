f13314508009f4ba96db4961a8e58d5263dde706
Richard Heycock <rgh@roughage.com.au>
2012-03-10 15:52:47 +1100

    Add the models

---
+++ lib/models.rb
@@ -0,0 +1,35 @@
+require 'sequel'
+require 'htmlcleaner'
+require 'addressable/uri'
+
+Sequel.connect('postgres:///bookmarker_development')
+
+class Document < Sequel::Model
+  one_to_many :document_downloads
+
+  def before_create
+    if self.html
+      cleaner = HtmlCleaner.new(self.html)
+      self.content = cleaner.content
+    end
+
+    now = Time.now.utc
+    self.created_at ||= now
+    self.updated_at = now
+    self.domain = Addressable::URI.parse(self.uri).host
+    super
+  end
+end
+
+class DocumentDownload < Sequel::Model
+  many_to_one :documents
+  many_to_one :users
+
+  def before_create
+    self.download_at = Time.now.utc
+  end
+end
+
+class User < Sequel::Model
+  one_to_many :document_downloads
+end
