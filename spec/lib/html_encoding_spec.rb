687c77974888fab884d405bc65c464774157f8a3
Richard Heycock <rgh@filterfish.org>
2012-12-27 14:54:53 +1100

    Move spec data to the spec dir. Minor cleanup.

--- spec/lib/html_encoding_spec.rb
+++ spec/lib/html_encoding_spec.rb
@@ -3,20 +3,24 @@
 require 'spec_helper'
 require 'html_encoding'
 
+TEST_DATA_PATH = Pathname.new(File.expand_path("../../data", __FILE__))
+
 describe HtmlEncoding do
+
+  def test_file(file)
+    TEST_DATA_PATH.join(file).read(:encoding => 'ASCII-8BIT')
+  end
 
   before(:all) do
     @html_encoder = HtmlEncoding.new
-    @test_data_path = Pathname.new(File.expand_path("../../../tests/data", __FILE__))
   end
 
-  # it 'should properly encoding an ISO-8859-1 string' do
-  #   data = @test_data_path.join('iso-8859-1', 'forum-23.html').read(:encoding => 'ASCII-8BIT')
-  #   @html_encoder.encode(data)
-  # end
+  it 'should properly encoding an ISO-8859-1 string' do
+    @html_encoder.encode(test_file('iso-8859-1/forum-23.html'))
+  end
 
   it 'should properly encoding a uft-8 string' do
-    data = @test_data_path.join('utf-8', 'nginx-default').read(:encoding => 'ASCII-8BIT')
+    data = test_file('utf-8/nginx-default')
     @html_encoder.encode(data).should == data.encode('utf-8')
   end
 end
