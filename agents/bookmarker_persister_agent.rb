68646a2de342b0ea55fc8ab797d9c80b23f733f6
Richard Heycock <rgh@filterfish.org>
2013-03-09 14:59:12 +1100

    Log the username.

--- agents/bookmarker_persister_agent.rb
+++ agents/bookmarker_persister_agent.rb
@@ -38,16 +38,16 @@ class BookmarkerPersisterAgent < Smith::Agent
         case document.status
         when 200,400..499
           user.document_downloads << DocumentDownload.create(:user_id => user.id, :document_id => document.id)
-          logger.info("DocumentDownload updated: [#{user.login}]: #{payload.uri}")
+          logger.info("Document updated: [#{user.login}]: #{payload.uri}")
         when 500..599
-          download_document(payload.uri) do |response|
+          download_document(payload.uri, user) do |response|
             document.update_fields(response, [:uri, :redirected_uri, :status, :html])
           end
         else
           logger.error { "FIXME. Something should go here document: #{document}" }
         end
       else
-        download_document(payload.uri) do |response|
+        download_document(payload.uri, user) do |response|
           begin
             document = Document.create(response)
             user.document_downloads << DocumentDownload.create(:user_id => user.id, :document_id => document.id)
@@ -60,18 +60,19 @@ class BookmarkerPersisterAgent < Smith::Agent
     end
   end
 
-  def download_document(uri, &blk)
+  def download_document(uri, user, &blk)
     begin
       logger.debug("Downloading: #{uri.display}")
 
       http = EM::HttpRequest.new(uri).get(:connect_timeout => 120, :inactivity_timeout => 240, :redirects => 12, :head => {"accept-encoding" => "gzip, compressed"})
       http.callback do
-        logger.info("Document downloaded: #{http.response_header.status}  #{http.last_effective_url.to_s}")
+        logger.info("Document downloaded: [#{user.login}]: #{http.response_header.status}  #{http.last_effective_url.display_uri}")
+
         blk.call(:uri => uri, :redirected_uri => http.last_effective_url, :status => http.response_header.status, :html => @html_encoder.encode(http.response, http.response_header['Content-Type']))
       end
 
       http.errback do
-        logger.warn("Document could not be downloaded: #{uri}")
+        logger.info("Document could not be downloaded: [#{user.login}]: #{http.response_header.status}  #{uri}")
       end
     rescue Addressable::URI::InvalidURIError => e
       logger.error("Invalid uri: #{uri.display}")
